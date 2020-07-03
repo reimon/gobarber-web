2243b4a693eb38965c2eae07f6171b99019882a2
Reimon Ferreira <reimonf@gmail.com>
2020-07-02 22:51:15 -0300

    :rocket: Rotas autenticadas 

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
-// import Route from './Route';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 // import ForgotPassword from '../pages/ForgotPassword';
 // import ResetPassword from '../pages/ResetPassword';
 
-// import Dashboard from '../pages/Dashboard';
+import Dashboard from '../pages/Dashboard';
 // import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
+
     {/* <Route path="/forgot-password" component={ForgotPassword} />
     <Route path="/reset-password" component={ResetPassword} />
 
