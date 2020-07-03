cbe139b7fc9b4a7d832a1c722b50d9cf174bb709
Reimon Ferreira <reimonf@gmail.com>
2020-07-03 08:41:50 -0300

    :rocket: Fist part finished 

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -64,7 +64,7 @@ const SignIn: React.FC = () => {
         addToast({
           type: 'error',
           title: 'Erro na autenticação',
-          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais!',
         });
       }
     },
