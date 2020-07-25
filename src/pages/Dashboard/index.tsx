144d671f94accd74377ed4ed62f0e3c757a7321a
Reimon Ferreira <reimonf@gmail.com>
2020-07-25 09:23:47 -0300

    :rocket: Próximo agendamento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,66 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import {
+  Container,
+  Header,
+  Content,
+  HeaderContent,
+  Calendar,
+  Profile,
+  Schedule,
+  Section,
+  Appointment,
+  NextAppointment,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { FiPower, FiClock } from 'react-icons/fi';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="Gobarber" />
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-Vindo</span>
+              <a>{user.name}</a>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-Feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a sequir</strong>
+            <div>
+              <img src={user.avatar_url} alt={user.name} />
+              <strong>{user.name}</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar></Calendar>
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
